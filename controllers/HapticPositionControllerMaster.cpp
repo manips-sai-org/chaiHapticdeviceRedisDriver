547000902dc878475fa98561e79ede6caa97651e
Mikael <mjorda@stanford.edu>
2020-07-08 12:07:58 -0700

    added simple local autonomy controllers

--- controllers/HapticPositionControllerMaster.cpp
+++ controllers/HapticPositionControllerMaster.cpp
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
 
 	if(argc < 2)
 	{
-		cout << "Please enter the device name as an argument ('device0' or 'device1')" << endl;
+		cout << "Please enter the device name as an argument ('device0' or 'device1' or 'device0-remote')" << endl;
 		return -1;
 	}
 	string device_name = argv[1];
@@ -64,9 +64,22 @@ int main(int argc, char** argv) {
 
 		DEVICE_COMMANDED_FORCE_KEY = "sai2::ChaiHapticDevice::device1::actuators::commanded_force";		     
 	}
+	if(device_name == "device0-remote")
+	{
+		DEVICE_MAX_STIFFNESS_KEY = "sai2::ChaiHapticDevice::device0::specifications::max_stiffness";
+		DEVICE_MAX_DAMPING_KEY = "sai2::ChaiHapticDevice::device0::specifications::max_damping";
+		DEVICE_MAX_FORCE_KEY = "sai2::ChaiHapticDevice::device0::specifications::max_force";
+
+		DEVICE_POSITION_KEY = "sai2::ChaiHapticDevice::device0::sensors::current_position";
+		DEVICE_TRANS_VELOCITY_KEY = "sai2::ChaiHapticDevice::device0::sensors::current_trans_velocity";
+		DESIRED_DEVICE_POSITION_KEY = "Sai2::ChaiHapticDevice::PositionController::device0::desired_position";
+		DESIRED_DEVICE_LINEAR_VELOCITY_KEY = "Sai2::ChaiHapticDevice::PositionController::device0::desired_linear_velocity";
+
+		DEVICE_COMMANDED_FORCE_KEY = "sai2::ChaiHapticDevice::device0::actuators::commanded_force";
+	}	
 	else
 	{
-		cout << "argument must be 'device0' or 'device1'" << endl;
+		cout << "argument must be 'device0' or 'device1' or 'device0-remote'" << endl;
 		return -1;       
 	}
 
@@ -76,36 +89,40 @@ int main(int argc, char** argv) {
 	signal(SIGINT, &sighandler);
 
 	// start redis client
-	auto redis_client = RedisClient();
-	redis_client.connect();
+	auto redis_client_local = RedisClient();
+	redis_client_local.connect();
+
+	const string remote_ip = "10.0.0.244";
+	auto redis_client_remote = RedisClient();
+	redis_client_remote.connect(remote_ip);
 
 	// setup position controller
 	// const double workspace_radius = 0.05;
-	// double max_force = redis_client.getEigenMatrixJSON(DEVICE_MAX_FORCE_KEY)(0);
+	// double max_force = redis_client_local.getEigenMatrixJSON(DEVICE_MAX_FORCE_KEY)(0);
 
 	Vector3d device_command_force = Vector3d::Zero();
 
 	Vector3d device_desired_position = Vector3d::Zero();
 	Vector3d device_desired_linear_velocity = Vector3d::Zero();
 
-	// redis_client.setEigenMatrixJSON(DESIRED_DEVICE_POSITION_KEY, device_desired_position);
-	// redis_client.setEigenMatrixJSON(DESIRED_DEVICE_LINEAR_VELOCITY_KEY, device_desired_linear_velocity);
+	// redis_client_local.setEigenMatrixJSON(DESIRED_DEVICE_POSITION_KEY, device_desired_position);
+	// redis_client_local.setEigenMatrixJSON(DESIRED_DEVICE_LINEAR_VELOCITY_KEY, device_desired_linear_velocity);
 
-	Vector3d device_position = redis_client.getEigenMatrixJSON(DEVICE_POSITION_KEY);
-	Vector3d device_velocity = redis_client.getEigenMatrixJSON(DEVICE_TRANS_VELOCITY_KEY);
+	Vector3d device_position = redis_client_local.getEigenMatrixJSON(DEVICE_POSITION_KEY);
+	Vector3d device_velocity = redis_client_local.getEigenMatrixJSON(DEVICE_TRANS_VELOCITY_KEY);
 
-	// double kp = 0.07 * redis_client.getEigenMatrixJSON(DEVICE_MAX_STIFFNESS_KEY)(0);
-	// double kv = 0.55 * redis_client.getEigenMatrixJSON(DEVICE_MAX_DAMPING_KEY)(0);
+	// double kp = 0.07 * redis_client_local.getEigenMatrixJSON(DEVICE_MAX_STIFFNESS_KEY)(0);
+	// double kv = 0.55 * redis_client_local.getEigenMatrixJSON(DEVICE_MAX_DAMPING_KEY)(0);
 
 	// setup redis reads and writes
-	redis_client.createReadCallback(0);
+	redis_client_local.createReadCallback(0);
 
-	redis_client.addEigenToReadCallback(0, DEVICE_POSITION_KEY, device_position);
-	redis_client.addEigenToReadCallback(0, DEVICE_TRANS_VELOCITY_KEY, device_velocity);
+	redis_client_local.addEigenToReadCallback(0, DEVICE_POSITION_KEY, device_position);
+	redis_client_local.addEigenToReadCallback(0, DEVICE_TRANS_VELOCITY_KEY, device_velocity);
 
-	redis_client.createWriteCallback(0);
-	redis_client.addEigenToWriteCallback(0, DESIRED_DEVICE_POSITION_KEY, device_desired_position);
-	redis_client.addEigenToWriteCallback(0, DESIRED_DEVICE_LINEAR_VELOCITY_KEY, device_desired_linear_velocity);
+	redis_client_remote.createWriteCallback(0);
+	redis_client_remote.addEigenToWriteCallback(0, DESIRED_DEVICE_POSITION_KEY, device_desired_position);
+	redis_client_remote.addEigenToWriteCallback(0, DESIRED_DEVICE_LINEAR_VELOCITY_KEY, device_desired_linear_velocity);
 
 	//// Create a timer ////
 	LoopTimer timer;
@@ -124,13 +141,13 @@ int main(int argc, char** argv) {
 		double time = timer.elapsedTime() - start_time;
 		dt = current_time - prev_time;
 
-		redis_client.executeReadCallback(0);
+		redis_client_local.executeReadCallback(0);
 
 		// transmit desired position and velocity
 		device_desired_position = device_position;
 		device_desired_linear_velocity = device_velocity;
 
-		redis_client.executeWriteCallback(0);
+		redis_client_remote.executeWriteCallback(0);
 
 	}
 
